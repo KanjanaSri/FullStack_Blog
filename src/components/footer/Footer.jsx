ae10e2f315e15a447e199ea76133a3df524c9122
SKarnjana <ksrisumrarn.pam@gmail.com>
2023-09-24 16:12:56 -0700

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <div>&copy;2023 Wasser. All rights reserved.</div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Footer;
